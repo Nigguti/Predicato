a4e83c033067e37da4194007a1cae2476fca06be
Niguti <niguti22@gmail.com>
2024-10-21 11:48:47 -0300

    funcao_lingua

--- index.py
+++ index.py
@@ -11,23 +11,21 @@ pessoas = [
      Pessoa(nome='Maria', nacao='brasileira', lingua='português')
    
 ]
-#regras
-for pessoa in pessoas:
+#regras 
+def listaLinguistica():
+ for pessoa in pessoas:
      if pessoa.lingua == 'português' and (pessoa.nacao == 'brasileiro' or pessoa.nacao == 'brasileira'):
-         print(f'{pessoa.nome} é do Brasil')
-     else:
-         print(f'{pessoa.nome} não é do Brasil')
+         print(f'\t{pessoa.nome} é {pessoa.nacao}')
 
-for pessoa in pessoas:
+ for pessoa in pessoas:
     if pessoa.lingua == 'inglês' and (pessoa.nacao == 'americano' or pessoa.nacao == 'americana'):
-        print(f'{pessoa.nome} é do EUA')
-    else:
-        print(f'{pessoa.nome} não é do EUA')
+        print(f'\t{pessoa.nome} é {pessoa.nacao}')
 
-for pessoa in pessoas:
+ for pessoa in pessoas:
     if pessoa.lingua == 'espanhol' and pessoa.nacao == 'espanhol':
-        print(f'{pessoa.nome} é da Espanha')
-    else:
-        print(f'{pessoa.nome} não é da Espanha')
+        print(f'\t{pessoa.nome} é {pessoa.nacao}')
 
+print('\n\tLista das Pessoas:\n')
+listaLinguistica()
+print('\n')
 
