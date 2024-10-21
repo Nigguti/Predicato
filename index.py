f9a95e9cb43b5e7ec7f803d8286c7a84de9df5ba
Niguti <niguti22@gmail.com>
2024-10-21 11:27:13 -0300

    regras

--- index.py
+++ index.py
@@ -7,6 +7,27 @@ class Pessoa:
 pessoas = [
      Pessoa(nome='João', nacao='brasileiro', lingua='português'),
      Pessoa(nome='Anna', nacao='americana', lingua='inglês'),
-     Pessoa(nome='Carlos', nacao='espanhol', lingua='espanho'),
+     Pessoa(nome='Carlos', nacao='espanhol', lingua='espanhol'),
      Pessoa(nome='Maria', nacao='brasileira', lingua='português')
+   
 ]
+#regras
+for pessoa in pessoas:
+     if pessoa.lingua == 'português' and (pessoa.nacao == 'brasileiro' or pessoa.nacao == 'brasileira'):
+         print(f'{pessoa.nome} é do Brasil')
+     else:
+         print(f'{pessoa.nome} não é do Brasil')
+
+for pessoa in pessoas:
+    if pessoa.lingua == 'inglês' and (pessoa.nacao == 'americano' or pessoa.nacao == 'americana'):
+        print(f'{pessoa.nome} é do EUA')
+    else:
+        print(f'{pessoa.nome} não é do EUA')
+
+for pessoa in pessoas:
+    if pessoa.lingua == 'espanhol' and pessoa.nacao == 'espanhol':
+        print(f'{pessoa.nome} é da Espanha')
+    else:
+        print(f'{pessoa.nome} não é da Espanha')
+
+
